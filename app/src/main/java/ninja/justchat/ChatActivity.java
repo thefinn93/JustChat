8615fee5391bc93a3d3dcd4ce8ff6eef558e8db4
Finn <finn@finn.io>
2015-05-19 11:15:44 -0700

    Fixed @CD-Rom321's build-breaking changes. Still lots of fuckery to deal with...

--- app/src/main/java/ninja/justchat/ChatActivity.java
+++ app/src/main/java/ninja/justchat/ChatActivity.java
@@ -80,13 +80,13 @@ public class ChatActivity extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+//                mTitle = getString(R.string.title_section1);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+//                mTitle = getString(R.string.title_section2);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+//                mTitle = getString(R.string.title_section3);
                 break;
         }
     }
